1cbdde8730af657ab409f375dcb407f70f57e4e1
Bikerlo <kmilo0910@gmail.com>
2023-10-11 16:33:03 -0500

    se crean los modelos del patron DAO, un objeto para cada tabla, asi como sus atributos y sus respectivos getters and setters

---
+++ src/main/java/com/mycompany/models/goal.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.models;
+
+/**
+ *
+ * @author torre
+ */
+public class goal {
+    private int ID;
+    private String player;
+    private String match;
+    private String description;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getMatch() {
+        return match;
+    }
+
+    public void setMatch(String match) {
+        this.match = match;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
